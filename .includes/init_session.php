0a8e44173ebc80234f0812302fad966545869523
fathmasetia <fathma.setia10@smk.belajar.id>
2025-02-04 12:07:28 +0700

    tambah

--- .includes/init_session.php
+++ .includes/init_session.php
@@ -1,10 +1,21 @@
 <?php
 session_start();
 
+$userId = $_SESSION['user_id'];
 $name = $_SESSION["name"];
 $role = $_SESSION["role"];
 
 $notification = $_SESSION['notification'] ?? null;
 if ($notification) {
     unset($_SESSION['notification']);
+}
+/* periksa apakah sesi username dan role sudah ada,
+jika tidak arahkan ke halaman login */
+if (empty($_SESSION["username"]) || empty($_SESSION["role"])) {
+    $_SESSION['notification'] = [
+        'type' => 'danger',
+        'message' => 'Silahkan Login Terlebih Dahulu!'
+    ];
+    header('Location: ./auth/login.php');
+    exit(); // pastikan script berhenti setelah pengalihan
 }
